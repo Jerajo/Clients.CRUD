4d488dca1fb543e30c7a5083e8a619037238c48e
= <=>
2021-03-02 18:15:50 -0400

    base entity completed

--- Source/Clients.Core/Models/BaseEntity.cs
+++ Source/Clients.Core/Models/BaseEntity.cs
@@ -1,7 +1,22 @@
-﻿namespace Clients.Core.Models
+﻿using System;
+
+namespace Clients.Core.Models
 {
     public abstract class BaseEntity<T> : Contracts.Entity<T>
     {
+        /// <inheritdoc/>
         public T Id { get; set; }
+
+        /// <inheritdoc/>
+        public T CreatedBy { get; set; }
+
+        /// <inheritdoc/>
+        public DateTime CreatedDate { get; set; }
+
+        /// <inheritdoc/>
+        public T UpdatedBy { get; set; }
+
+        /// <inheritdoc/>
+        public DateTime UpdatedDate { get; set; }
     }
 }
