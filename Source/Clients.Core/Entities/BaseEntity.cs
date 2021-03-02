3d0ec8739fe97331e41f8e712215f1a0944548d0
= <=>
2021-03-02 19:14:59 -0400

    Base DTO completed

--- Source/Clients.Core/Entities/BaseEntity.cs
+++ Source/Clients.Core/Entities/BaseEntity.cs
@@ -2,6 +2,10 @@
 
 namespace Clients.Core.Entities
 {
+    /// <summary>
+    /// Represents a entity with the minimum information for the database.
+    /// </summary>
+    /// <typeparam name="T">The entities id type.</typeparam>
     public abstract class BaseEntity<T> : Contracts.Entity<T>
     {
         /// <inheritdoc/>
--- Source/Clients.SqlServer/DTOs/BaseDto.cs
+++ Source/Clients.SqlServer/DTOs/BaseDto.cs
@@ -1,7 +1,14 @@
 ﻿namespace Clients.SqlServer.DTOs
 {
+    /// <summary>
+    /// Represents a model with the minimum information.
+    /// </summary>
+    /// <typeparam name="T">The entities id type.</typeparam>
     public class BaseDto<T>
     {
+        /// <summary>
+        /// The entity unique identifier.
+        /// </summary>
         T Id { get; set; }
     }
 }
