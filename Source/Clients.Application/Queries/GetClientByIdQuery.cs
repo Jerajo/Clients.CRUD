e0489808afbda6e475d827b264d86ce6bc10c0f4
= <=>
2021-03-03 08:15:35 -0400

    Added clients queries.

---
+++ Source/Clients.Application/Queries/GetClientByIdQuery.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Ardalis.GuardClauses;
+using AutoMapper;
+using Clients.Application.DTOs;
+using Clients.Core.Contracts;
+using Clients.Core.Entities;
+
+namespace Clients.Application.Queries
+{
+    public class GetClientByIdQuery : BaseQuery<Func<Client, bool>, ClientDto>
+    {
+        private readonly IRepository<Client> _repository;
+        private readonly IMapper _mapper;
+
+        public GetClientByIdQuery(IRepository<Client> repository, IMapper mapper)
+        {
+            Guard.Against.Null(repository, nameof(repository));
+            Guard.Against.Null(mapper, nameof(mapper));
+
+            _repository = repository;
+            _mapper = mapper;
+        }
+
+        public override ClientDto Execute(Func<Client, bool> query)
+        {
+            Guard.Against.Null(query, nameof(query));
+
+            var client = _repository.Get(query);
+
+            var clientDto = _mapper.Map<ClientDto>(client);
+
+            return clientDto;
+        }
+    }
+}
---
+++ Source/Clients.Application/Queries/GetClientsQuery.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using Ardalis.GuardClauses;
+using AutoMapper;
+using Clients.Application.DTOs;
+using Clients.Core.Contracts;
+using Clients.Core.Entities;
+
+namespace Clients.Application.Queries
+{
+    public class GetClientsQuery : BaseQuery<Func<Client, bool>, List<ClientDto>>
+    {
+        private readonly IRepository<Client> _repository;
+        private readonly IMapper _mapper;
+
+        public GetClientsQuery(IRepository<Client> repository, IMapper mapper)
+        {
+            Guard.Against.Null(repository, nameof(repository));
+            Guard.Against.Null(mapper, nameof(mapper));
+
+            _repository = repository;
+            _mapper = mapper;
+        }
+
+        public override List<ClientDto> Execute(Func<Client, bool> query)
+        {
+            Guard.Against.Null(query, nameof(query));
+
+            var clients = _repository.Query(query);
+
+            var clientsDto = _mapper.Map<List<ClientDto>>(clients);
+
+            return clientsDto;
+        }
+    }
+}
