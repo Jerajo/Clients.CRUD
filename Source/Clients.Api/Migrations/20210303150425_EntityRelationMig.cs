11dd8e19f74c4e50cb04ddbd49610fa2f464f5ff
= <=>
2021-03-03 11:41:35 -0400

    Added relation with the client and address entities.

---
+++ Source/Clients.Api/Migrations/20210303150425_EntityRelationMig.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Clients.Api.Migrations
+{
+    public partial class EntityRelationMig : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Addresses_Clients_ClientId",
+                table: "Addresses");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "ClientId",
+                table: "Addresses",
+                nullable: false,
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Addresses_Clients_ClientId",
+                table: "Addresses",
+                column: "ClientId",
+                principalTable: "Clients",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Addresses_Clients_ClientId",
+                table: "Addresses");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "ClientId",
+                table: "Addresses",
+                type: "uniqueidentifier",
+                nullable: true,
+                oldClrType: typeof(Guid));
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Addresses_Clients_ClientId",
+                table: "Addresses",
+                column: "ClientId",
+                principalTable: "Clients",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
