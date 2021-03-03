64baf80712ee41227765e4e8791ca552cd5ea288
= <=>
2021-03-03 08:16:59 -0400

    Added Address queries

---
+++ Source/Clients.Application/Queries/GetAddressByIdQuery.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Ardalis.GuardClauses;
+using AutoMapper;
+using Clients.Application.DTOs;
+using Clients.Core.Contracts;
+using Clients.Core.Entities;
+
+namespace Clients.Application.Queries
+{
+    public class GetAddressByIdQuery : BaseQuery<Func<Address, bool>, AddressDto>
+    {
+        private readonly IRepository<Address> _repository;
+        private readonly IMapper _mapper;
+
+        public GetAddressByIdQuery(IRepository<Address> repository, IMapper mapper)
+        {
+            Guard.Against.Null(repository, nameof(repository));
+            Guard.Against.Null(mapper, nameof(mapper));
+
+            _repository = repository;
+            _mapper = mapper;
+        }
+
+        public override AddressDto Execute(Func<Address, bool> query)
+        {
+            Guard.Against.Null(query, nameof(query));
+
+            var address = _repository.Get(query);
+
+            var addressDto = _mapper.Map<AddressDto>(address);
+
+            return addressDto;
+        }
+    }
+}
