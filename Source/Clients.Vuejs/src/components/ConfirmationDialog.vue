e5bc81472faab4f54d7ee6105e03640d917cb1d0
= <=>
2021-03-03 12:33:41 -0400

    components created

---
+++ Source/Clients.Vuejs/src/components/ConfirmationDialog.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="container">
+    <h1>Hello World</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+
+@Component
+export default class ConfirmationDialog extends Vue {
+  @Prop() private msg!: string;
+}
+</script>
