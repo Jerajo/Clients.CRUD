7bf1e19080d5eda42c7a2efc4057aa69e5b1c0fc
= <=>
2021-03-03 10:46:32 -0400

    Added inicial migration

---
+++ Source/Clients.Api/Migrations/20210303124556_InitialMigration.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Clients.Api.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Clients",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    CreatedBy = table.Column<Guid>(nullable: false),
+                    CreatedDate = table.Column<DateTime>(nullable: false),
+                    UpdatedBy = table.Column<Guid>(nullable: false),
+                    UpdatedDate = table.Column<DateTime>(nullable: false),
+                    DeleteFlag = table.Column<string>(nullable: true),
+                    FullName = table.Column<string>(maxLength: 150, nullable: false),
+                    UserName = table.Column<string>(maxLength: 35, nullable: false),
+                    Email = table.Column<string>(maxLength: 150, nullable: false),
+                    BirthDay = table.Column<DateTime>(nullable: false),
+                    MarriageStatus = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Clients", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Addresses",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    CreatedBy = table.Column<Guid>(nullable: false),
+                    CreatedDate = table.Column<DateTime>(nullable: false),
+                    UpdatedBy = table.Column<Guid>(nullable: false),
+                    UpdatedDate = table.Column<DateTime>(nullable: false),
+                    DeleteFlag = table.Column<string>(nullable: true),
+                    Country = table.Column<string>(maxLength: 100, nullable: false),
+                    State = table.Column<string>(maxLength: 100, nullable: false),
+                    City = table.Column<string>(maxLength: 100, nullable: false),
+                    PostalCode = table.Column<decimal>(type: "decimal(5,0)", nullable: false),
+                    AddressLineOne = table.Column<string>(maxLength: 250, nullable: false),
+                    AddressLineTwo = table.Column<string>(maxLength: 250, nullable: false),
+                    ClientId = table.Column<Guid>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Addresses", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Addresses_Clients_ClientId",
+                        column: x => x.ClientId,
+                        principalTable: "Clients",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Addresses_ClientId",
+                table: "Addresses",
+                column: "ClientId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Clients_UserName",
+                table: "Clients",
+                column: "UserName",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Addresses");
+
+            migrationBuilder.DropTable(
+                name: "Clients");
+        }
+    }
+}
