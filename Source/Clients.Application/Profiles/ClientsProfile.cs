649ac6d6d2cb4298f6ce537e50d11785ac0eb327
= <=>
2021-03-02 23:13:44 -0400

    Added AutoMapper and EntitiesDtos Profiles

---
+++ Source/Clients.Application/Profiles/ClientsProfile.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using Clients.Application.DTOs;
+using Clients.Core.Entities;
+
+namespace Clients.Application.Profiles
+{
+    /// <summary>
+    /// ClientDto mapping configuration.
+    /// </summary>
+    class ClientsProfile : Profile
+    {
+        public ClientsProfile()
+        {
+            CreateMap<ClientDto, Client>()
+                .ForMember(client => client.Id, action => action.Ignore());
+
+            CreateMap<Client, ClientDto>();
+        }
+    }
+}
