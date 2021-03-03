2ce0aba6353b938e6f670449f25c891d3015a525
= <=>
2021-03-03 10:50:07 -0400

    Added Configurations For the API project

---
+++ Source/Clients.Api/ApiConfig.cs
@@ -0,0 +1,45 @@
+﻿using Clients.Api.Factories;
+using Clients.Application.Commands;
+using Clients.Application.Queries;
+using Clients.Core.Contracts;
+using Clients.SqlServer.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Clients.Api
+{
+    public static class ApiConfig
+    {
+        public static void ConfigIoCForCommands(this IServiceCollection services)
+        {
+            services.AddScoped<CreateClientCommand>();
+            services.AddScoped<DeleteClientCommand>();
+            services.AddScoped<UpdateClientCommand>();
+
+            services.AddScoped<CreateAddressCommand>();
+            services.AddScoped<DeleteAddressCommand>();
+            services.AddScoped<UpdateAddressCommand>();
+        }
+
+        public static void ConfigIoCForQueries(this IServiceCollection services)
+        {
+            services.AddScoped<GetClientsQuery>();
+            services.AddScoped<GetClientByIdQuery>();
+
+            services.AddScoped<GetAddressesQuery>();
+            services.AddScoped<GetAddressByIdQuery>();
+        }
+
+        public static void ConfigIoCForFactories(this IServiceCollection services)
+        {
+            services.AddScoped<ICommandFactory, CommandFactory>();
+            services.AddScoped<IQueryFactory, QueryFactory>();
+            services.AddScoped<IRepositoryFactory, RepositoryFactory>();
+        }
+
+        public static void ConfigIoCServices(this IServiceCollection services)
+        {
+            services.AddScoped(typeof(IRepository<>), typeof(DataRepository<>));
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+        }
+    }
+}
