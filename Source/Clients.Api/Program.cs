a3fa3fe66a8f33e886dceecdb005e9d479e42a69
jerajo <jessejose@outlook.es>
2021-03-05 20:33:51 -0400

    sqlitle mock configuration added

--- Source/Clients.Api/Program.cs
+++ Source/Clients.Api/Program.cs
@@ -1,11 +1,13 @@
-using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
+using Clients.SqlServer;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+
+using Serilog;
+using Serilog.Events;
+using Serilog.Sinks.SystemConsole.Themes;
 
 namespace Clients.Api
 {
@@ -13,7 +15,37 @@ namespace Clients.Api
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
+                .MinimumLevel.Override("System", LogEventLevel.Warning)
+                .MinimumLevel.Override("Microsoft.AspNetCore.Authentication", LogEventLevel.Information)
+                .WriteTo.Console(outputTemplate: "[{Timestamp:HH:mm:ss} {Level}] {SourceContext}{NewLine}{Message:lj}{NewLine}{Exception}{NewLine}", theme: AnsiConsoleTheme.Literate)
+                .WriteTo.File("serverAPI_e_logs", Serilog.Events.LogEventLevel.Error, outputTemplate: "{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} [{Level:u3}] {Message:lj}{NewLine}{Exception}")
+                .CreateLogger();
+
+            try
+            {
+                Log.Information("Building host...");
+                var host = CreateHostBuilder(args).Build();
+                Log.Information("Host builded.");
+
+                if (args.Any(x => x.Contains("MOCK")))
+                    MigrateDataBase(host);
+
+                Log.Information("Host runing...");
+                host.Run();
+            }
+            catch (System.Exception ex)
+            {
+                Log.Fatal("--Host stopped: {0}  \n\n --InnerException: {1}",
+                    ex.Message,
+                    ex.InnerException);
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
@@ -21,6 +53,23 @@ namespace Clients.Api
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
+                    webBuilder.UseSerilog();
                 });
+
+        public static void MigrateDataBase(IHost host)
+        {
+            using (var serviceScope = host.Services.CreateScope())
+            {
+                var services = serviceScope.ServiceProvider;
+                var dbConfigContext = services.GetRequiredService<ApplicationDBContext>();
+
+                Log.Information("Migratting SQLite database...");
+                if (!dbConfigContext.Database.EnsureCreated())
+                {
+                    dbConfigContext.Database.Migrate();
+                }
+                Log.Information("SQLite database Migrated.");
+            }
+        }
     }
 }
