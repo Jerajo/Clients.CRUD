9550cbcfef75208e41ef65cbf9cc0e1c8a033cb9
jerajo <jessejose@outlook.es>
2021-03-07 19:31:21 -0400

    get address now exclude delete flagged

--- Source/Clients.Application/Queries/GetAddressByQuery.cs
+++ Source/Clients.Application/Queries/GetAddressByQuery.cs
@@ -7,12 +7,12 @@ using Clients.Core.Entities;
 
 namespace Clients.Application.Queries
 {
-    public class GetAddressByIdQuery : BaseQuery<Func<Address, bool>, AddressDto>
+    public class GetAddressByQuery : BaseQuery<Func<Address, bool>, AddressDto>
     {
         private readonly IRepository<Address> _repository;
         private readonly IMapper _mapper;
 
-        public GetAddressByIdQuery(IRepository<Address> repository, IMapper mapper)
+        public GetAddressByQuery(IRepository<Address> repository, IMapper mapper)
         {
             Guard.Against.Null(repository, nameof(repository));
             Guard.Against.Null(mapper, nameof(mapper));
