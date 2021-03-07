a0cf67bf644d1b7952acf29edf73164c941402a4
jerajo <jessejose@outlook.es>
2021-03-06 21:57:43 -0400

    bootstrap vue added

--- Source/Clients.Vuejs/src/main.ts
+++ Source/Clients.Vuejs/src/main.ts
@@ -1,8 +1,19 @@
 import Vue from "vue";
 import App from "./App.vue";
 import "./registerServiceWorker";
+
 import router from "./router";
 import store from "./store";
+
+// Bootstrap and Bootstrap-Vue
+import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
+Vue.use(BootstrapVue);
+Vue.use(IconsPlugin);
+
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
+
+// Vee-Validate Rules and
 import * as rules from "../node_modules/vee-validate/dist/rules";
 import { ValidationObserver, extend, ValidationProvider } from "vee-validate";
 
@@ -13,7 +24,6 @@ for (const [rule, validation] of Object.entries(rules)) {
 }
 
 Vue.component("ValidationProvider", ValidationProvider);
-
 Vue.component("ValidationObserver", ValidationObserver);
 
 Vue.config.productionTip = false;
